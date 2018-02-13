d5bc0140a57ae1e0a44808d8dbd301bdd9425cc5
Ben <benadamsdroid@gmail.com>
2018-02-13 00:30:14 -0800

    changes to get package build working

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const electron = require('electron');
 const { setMainMenu } = require('./main-menu');
 const { parseLog } = require('./logwatch');
 const { setStorage } = require('./setStorage');
-require('electron-debug')({showDevTools: true});
+//require('electron-debug')({showDevTools: true});
 const storage = require('electron-json-storage');
 const ipc = electron.ipcMain;
 var watch = require('node-watch');
@@ -17,7 +17,8 @@ app.on('ready', () => {
     show: false,
     resizable: true,
     width: 1000,
-    height: 900
+    height: 720,
+    icon: path.join(__dirname, 'icon_64x64.png')
   });
   mainWindow.loadURL(path.join('file://', __dirname, 'index.html'));
   setMainMenu(mainWindow);
