161edd428c4788aab4c54603907bd8326b6c07fe
Taylor <taylor.moss@gmail.com>
2019-04-27 13:37:04 -0600

    Added airbnb, setup some classes and some component did mount logic.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { ReactText } from 'react';
 import Home from './Home';
 import Help from './Help';
 import { MemoryRouter, Switch, Route } from 'react-router';
 import '../assets/css/global.css'
 
-const App = () => (
-  <MemoryRouter>
-    <Switch>
-      <Route path="/help" component={Help} />
-      <Route path="/" component={Home} />
-    </Switch>
-  </MemoryRouter>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    console.log('App Started');
+    // call api
+    this.setState({ loading: true });
+  }
+
+  render() {
+    return (
+      <MemoryRouter>
+        <Switch>
+          <Route path="/help" component={Help} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </MemoryRouter>
+    );
+  }
+}
 
 export default App;
