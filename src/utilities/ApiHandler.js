eb277959570f2f41afe3b86f9ceb5c149800b965
Anders Windell <anderswindell@gmail.com>
2019-04-27 14:39:37 -0600

    Adding Ben's code

--- src/utilities/ApiHandler.js
+++ src/utilities/ApiHandler.js
@@ -1,14 +1,42 @@
-import React from 'react';
 
-class ApiHandler extends React.Component {
-    componentDidMount() {
-    }
+
+exports.parseData = (playerIds) => {
+  console.log('Player Data', playerIds);
   
-    render() {
-      return (
-        
-      );
-    }
-  }
+  
+  // Return match data structure
+  return(null);
+}
 
-  export default ApiHandler
+
+
+// Code from Ben's V1 app
+// function getPlayer(playerID, slot, side) {
+//   let activePlayer = localStorage.getItem("activePlayer");
+//   if(activePlayer != "" && playerID != undefined) {
+//     if((side && slot >= 5) || (!side && slot < 5)){
+//       var apiRequest1 = fetch(`https://api.opendota.com/api/players/${activePlayer}/wl?against_account_id=${playerID}`).then(function(response){
+//           return response.json()
+//       });
+//     } else if ((side && slot < 5) || (!side && slot >=5)) {
+//       var apiRequest1 = fetch(`https://api.opendota.com/api/players/${activePlayer}/wl?with_account_id=${playerID}`).then(function(response){
+//           return response.json()
+//       });
+//     }
+//     var apiRequest2 = fetch(`https://api.opendota.com/api/players/${playerID}/recentMatches?significant=0`).then(function(response){
+//         return response.json()
+//     });
+//     var apiRequest3 = fetch(`https://api.opendota.com/api/players/${playerID}`).then(function(response){
+//         return response.json()
+//     });
+
+//     return Promise.all([apiRequest1,apiRequest2,apiRequest3]).then(function(values){
+//       namedObj = {};
+//       namedObj["wl"] = values[0]
+//       namedObj["matches"] = values[1]
+//       namedObj["player"] = values[2]
+//       return buildPlayer(namedObj, slot);
+//     });
+//   }
+// }
+
