4edc94feb927c98c129ca4ad390654b33cdb0041
Ben <benadamsdroid@gmail.com>
2019-09-09 14:37:19 -0600

    style tweaks

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -13,18 +13,21 @@ export default class PlayerCard extends Component {
   render() {
       const {cardData} = this.props;
       console.log(cardData);
-      console.log(process.env.PUBLIC_URL);
-      const Heroes = cardData.playerStats.heroes.splice(0, 3).map((hero, i) => {
+      const Heroes = cardData.playerStats.heroes.splice(0, 3).map((hero, i) => {        
       return (
-      <div key={`key-${i}`} className="recent-hero">
+      <div key={`key-${i}`} className={"recent-hero " + hero.name.toLowerCase().replace(/\s+/g, '-')}>
         <img src={hero.url} alt="" className="hero"/>
         <div className="hero-win-loss">
         <div className="hero-wl-inner">{hero.wins}-{hero.losses}</div>
         </div>
-      </div>)});
+      </div>)}
+      );
       const rankIcon = require('.' + cardData.rank_icon);
       const bgImg = require('./images/' + cardData.playerStats.cardBg);
       const bgStyle = {backgroundImage: `url(${bgImg})`};
+      const midPercent = Math.floor((cardData.playerStats.counts.mid/cardData.playerStats.counts.total)*100) + '%';
+      const safePercent = Math.floor((cardData.playerStats.counts.safelane/cardData.playerStats.counts.total)*100) + '%';
+      const offPercent = Math.floor((cardData.playerStats.counts.offlane/cardData.playerStats.counts.total)*100) + '%';
 
     return (
         <div className="player-card" style={bgStyle}>
@@ -43,7 +46,17 @@ export default class PlayerCard extends Component {
            <img src={rankIcon} alt=""/>
          </div>
          <div className="body-row">
-
+          <div className="description">
+            <div className="player-wrapper">
+              <p>Mid {midPercent}</p>
+              <p>Safelane {safePercent}</p>
+              <p>Offlane {offPercent}</p>
+              <p>Support:{cardData.playerStats.counts.support} Core:{cardData.playerStats.counts.core} Mid:{cardData.playerStats.counts.mid}</p>
+            </div>
+            <div className="hero-wrapper1">
+              <p>Ogre</p>
+            </div>
+          </div>
          </div>
          <div className="wl-row">
            <div className="win-loss">
